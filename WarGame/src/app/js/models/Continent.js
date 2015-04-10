b7818bc9ca50d940892020ab5600f79ed810deea
fabriciofelisbino <fabricio.felisbino@outlook.com>
2015-04-10 08:24:02 -0300

    Entidades

---
+++ WarGame/src/app/js/models/Continent.js
@@ -0,0 +1,18 @@
+﻿function Continent(name, territories){
+    this.name = name;
+    this.territories = territories;
+    this.isOccupiedBy = null;
+}
+
+Continent.prototype = {
+    name : null,
+    territories : [],
+    isOccupiedBy : null,
+
+    getTerritory : function (territoryName){
+        return null;
+    },
+    getIsOccupiedBy : function (){
+        return null;
+    },
+}
---
+++ WarGame/src/app/js/models/Territory.js
@@ -0,0 +1,18 @@
+﻿function Territory(name, neighbors, isOccupiedBy){
+    this.name = name;
+    this.neighbors = neighbors;
+    this.isOccupiedBy = null;
+}
+
+Territory.prototype = {
+    name : null,
+    neighbors : [],
+    isOccupiedBy : null,
+
+    getNeighbors : function (){
+        return null;
+    },
+    getIsOccupied : function (){
+        return null;
+    },
+}
